188c8132858e65b1af6255ef662c6e899e6606f2
Nawaraj Karki <nawarajkarkee404@gmail.com>
2024-05-03 17:11:39 +0545

    upgrade listener.py

--- listener.py
+++ listener.py
@@ -8,6 +8,8 @@ listener = APIRouter()
 async def webhook_listener():
     try:
         subprocess.run(["git", "pull"])
+        
+        subprocess.run(["/home/ec2-user/Testing-CI-CD-pipeline/cicd.sh"])
         return {"message": "Changes pulled successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to pull changes: {e}")
