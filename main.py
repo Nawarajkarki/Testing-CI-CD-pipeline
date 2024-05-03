508310f3119954a677f4b831221a8297a9f778e6
Nawaraj Karki <nawarajkarkee404@gmail.com>
2024-05-03 16:12:29 +0545

    CICD TEST1

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ app.include_router(listener)
 @app.get("/")
 def homepage():
     return {"msg" : "Welcome To the homepage of CICD test APP."}
+
+
+@app.get("/cicd")
+def cicd_test():
+    return {"msg" : "Display of this msg means success of the CICD pipeline"}
