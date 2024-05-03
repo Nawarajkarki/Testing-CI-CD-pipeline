5325734cd714ee12c159cade9b6e9717f23d70d1
Nawaraj Karki <nawarajkarkee404@gmail.com>
2024-05-03 16:02:20 +0545

    Webhook Listener added

---
+++ listener.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, HTTPException
+import subprocess
+
+listener = APIRouter()
+
+
+@listener.post("/listener")
+async def webhook_listener():
+    try:
+        subprocess.run(["git", "pull"])
+        return {"message": "Changes pulled successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to pull changes: {e}")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-
+from listener import listener
 
 app = FastAPI()
 
 
+app.include_router(listener)
+
 @app.get("/")
 def homepage():
-    return {"msg" : "Welcome To the homepage"}
+    return {"msg" : "Welcome To the homepage of CICD test APP."}
