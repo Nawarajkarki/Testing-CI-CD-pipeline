4f7e43a811d34f8b7734609b4c5bafe0abe17201
Nawaraj Karki <nawarajkarkee404@gmail.com>
2024-05-03 20:50:25 +0545

    lsadjkf

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.include_router(listener)
 
 @app.get("/")
 def homepage():
-    return {"msg" : "Requests added to the requirementes.txt file"}
+    return {"msg" : "Fucking work added to the requirementes.txt file"}
 
 
 @app.get("/dependency")
