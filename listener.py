8cc262f517213213ddd279dc02b831ff01c72163
Nawaraj Karki <nawarajkarkee404@gmail.com>
2024-05-03 20:34:41 +0545

    laskdjf

--- listener.py
+++ listener.py
@@ -9,7 +9,8 @@ async def webhook_listener():
     try:
         subprocess.run(["git", "pull"])
         
-        subprocess.run(["/home/ec2-user/Testing-CI-CD-pipeline/cicd.sh"])
+        subprocess.run(["bash", "/home/ec2-user/Testing-CI-CD-pipeline/cicd.sh"])
+        
         return {"message": "Changes pulled successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to pull changes: {e}")
